311023ade264168f8ad75933f3c3dbe483471818
TheMegaForge <themw@DESKTOP-ESODRO9>
2024-01-28 16:21:31 +0100

    added more HAL

--- os_src/stage1/HAL.c
+++ os_src/stage1/HAL.c
@@ -1,12 +1,14 @@
 #include "../include/HAL.h"
 #include "../include/i686/IDT.h"
-//#include "../include/i686/PIC.h"
+#include "../include/i686/IRQ.h"
 #include "../include/i686/ISR.h"
 extern void initPCIClasses();
 void HAL_Initilize(PCIBus* pciBus){
+    asm("cli");
     initPCIClasses();
     IDTInitialize();
-    //ISRInitialize();
+    ISRInitialize();
+    IRQInitialize();
     readPCIBus(pciBus);
 }
 
