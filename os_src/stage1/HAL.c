12b8154580d0b370403f3b0a63a284b467f6dd59
TheMegaForge <forgingsmeltery06@gmail.com>
2024-03-01 17:37:08 +0100

    Stage1 HAL mostly finished
    Added PCI Bus and IDE+ATA output

--- os_src/stage1/HAL.c
+++ os_src/stage1/HAL.c
@@ -2,13 +2,28 @@
 #include "../include/i686/IDT.h"
 #include "../include/i686/IRQ.h"
 #include "../include/i686/ISR.h"
+#include "../include/stdio.h"
 extern void initPCIClasses();
-void HAL_Initilize(PCIBus* pciBus){
+extern void setupDrivers();
+extern char** getTB();
+void HAL_Initilize(ATAController* controller,PCIBus* pciBus,IDEController* ide,StorageCommon* storage){
+    char** tb = getTB();
+    char* _tb = *tb;
     asm("cli");
     initPCIClasses();
     IDTInitialize();
     ISRInitialize();
     IRQInitialize();
+    setupDrivers();
     readPCIBus(pciBus);
+    for(int i=0;i<pciBus->devices;i++){
+        _PCIDevice* dev = &pciBus->PCIDevices[i];
+        if(dev->class.ClassCode == PC3_MASS_STORAGE_CONTROLLER && dev->class.desc.subClassID == 1){
+            setupIDEController(ide,dev);
+        }
+    }
+    readATAController(controller,ide);
+    ATAregister(storage);
+    asm("sti");
 }
 
