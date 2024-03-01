475bba8251233688fbba38f1e2a215f06aa7c9b8
TheMegaForge <forgingsmeltery06@gmail.com>
2024-03-01 17:34:46 +0100

    stage1 mostly finished
    removed some indev prints and incremnted the load count of stage2 to 97152 bytes.Checks for PCI Mek.

--- os_src/stage1/setup.c
+++ os_src/stage1/setup.c
@@ -6,6 +6,7 @@
 #include "../include/NativeTalk.h"
 #include "../include/PCI.h"
 #include "../include/HAL.h"
+#include "../include/i686/io.h"
 #include "../include/i686/math.h"
 #include "../include/utils/binary.h"
 #include "../include/i686/ISR.h"
@@ -19,10 +20,29 @@ void timer(Registers* regs){
 
 }
 void key(Registers* regs){
-    puts(&_tb,"KEY!",CC_WHITE_BLUE);
 }
 void switch_d(Registers* regs){
-
+    void** h_store = (void**)regs->eax;
+    h_store[0] = printf;
+    h_store[1] = newLine;
+    h_store[2] = puts;
+    h_store[3] = kpanic;
+    h_store[4] = setScreenColor;
+    h_store[5] = regDriverINT;
+    h_store[6] = getDriverINT;
+    h_store[7] = addInterruptHandler;
+    h_store[8] = IRQRegisterHandler;
+    h_store[9] = pciConfigReadDWord;
+    h_store[10] = pciConfigReadWord;
+    h_store[11] = readBar16;
+    h_store[12] = readBar32;
+    h_store[13] = strlen;
+    h_store[14] = memcpy;
+    h_store[15] = io_wait;
+    h_store[16] = inb_;
+    h_store[17] = ins_;
+    h_store[18] = outb_;
+    h_store[19] = outs_;
 }
 #define SETUP_COLOR CC_WHITE_BLUE
 extern int isrTest();
@@ -30,21 +50,14 @@ extern int isrTest();
 
 RawFunction __cdecl section(".setup")  void __setup(struct  NativeTalk* nt){
     _tb = nt->textAddress;
+    if(hasPCI2Mek()){
+        kpanic("CAN NOT BOOT!PC IS TOO OLD!");
+    }
     PCIBus        bus = {};
     IDEController ide = {};
     ATAController ata = {};
     StorageCommon storage = {};
     HAL_Initilize(&ata,&bus,&ide,&storage);
-    {
-        int w = 0;
-        newLine(&_tb,puts(&_tb,"IDE CHANNELS0/1->",CC_WHITE_BLUE),CC_WHITE_BLUE);
-        w = printf(_tb,"\tCHANNEL0,IOBase=%x,CntrBase=%x,INT=%d,MID=%d",CC_WHITE_BLUE,ide.channels[0].IObase,ide.channels[0].controlBase,ide.channels[0].INT,ide.channels[0].masterIDE);
-        _tb+=(w*2);
-        newLine(&_tb,w,CC_WHITE_BLUE);
-        w = printf(_tb,"\tCHANNEL1,IOBase=%x,CntrBase=%x,INT=%d,MID=%d",CC_WHITE_BLUE,ide.channels[1].IObase,ide.channels[1].controlBase,ide.channels[1].INT,ide.channels[1].masterIDE);
-        _tb+=(w*2);
-        newLine(&_tb,w,CC_WHITE_BLUE);
-    }
     IRQRegisterHandler(0,timer);
     IRQRegisterHandler(1,key);
     int w = printf(_tb,"ATAInfo: max LBA28 = %d,max LBA48(upper) = %d,lower = %d",CC_WHITE_BLUE,ata.info[0].maxLBA28,ata.info[0].MaxLba48.upper,ata.info[0].MaxLba48.lower);
@@ -71,7 +84,9 @@ RawFunction __cdecl section(".setup")  void __setup(struct  NativeTalk* nt){
     w = printf(_tb,"     KIB=%d,MIB=%d,GIB~=%d,raw_lower=%d,raw_upper=%d",CC_WHITE_BLUE,totalKIBS,totalMIBS,totalGIBS,KIB_lower,KIB_upper);
     _tb+=(w*2);
     newLine(&_tb,w,CC_WHITE_BLUE);
-    if(storage.read(&access,CCT_4048,1,4,0,0,STAGE_2_LOCATION) == CSE_SUCCESS){
+    // loads 97152 bytes 
+    if(storage.read(&access,CCT_12144,8,1,0,0,STAGE_2_LOCATION) == CSE_SUCCESS){
+        *TEXT_BUFFER = 'G';
         addInterruptHandler(100,switch_d);
         void(*stage2)(DiskInfo*,PCIBus*,StorageInfo*,char*) = STAGE_2_LOCATION;
         DiskInfo dInfo = {&ide,&ata,&storage};
@@ -79,7 +94,6 @@ RawFunction __cdecl section(".setup")  void __setup(struct  NativeTalk* nt){
         stage2(&dInfo,&bus,&si,_tb);
     }else{
         puts(&_tb,"CAN NOT BOOT!READING STAGE2 FAILED!",CC_WHITE_BLUE);
-        
     }
     for(;;){}
     return;
