da539cefc6e9dbb7d92a4ab489d1234408b8f5a9
TheMegaForge <forgingsmeltery06@gmail.com>
2024-03-03 00:13:01 +0100

    Clean up
    as it sound,Clean up

--- os_src/stage2/__pre.c
+++ os_src/stage2/__pre.c
@@ -5,41 +5,69 @@
 #include "../include/memdefs.h"
 #include "include/SwitchHandles.h"
 #include "include/PS2Controller.h"
+#include "include/Keyboard.h"
+#include "include/hw/KBDDriver.h"
 #include "include/x86.h"
 #include <stdint.h>
+int index = 0;
+static StageHandles* sh;
+static KBDDriver kbd_drv = {};
 __cdecl void _pstart(DiskInfo* dsk ,PCIBus* bus ,StorageInfo* storage,char* _tb){
     void* _f[20] = {};
     PS2Controller _ps2 = {};
-    getHandles(&_f[0]);
+    PS2Device _psd[2] = {};
     StageHandles h = {};
+    Keyboard kbd = {};
+    sh = &h;
+    getHandles(&_f[0]);
     setupHandles(&h,&_f[0]);
     h._tb = _tb;
     int w = h.puts(&h._tb,"Stage2 entered!",CC_WHITE_BLUE);
     h.newLine(&h._tb,w,CC_WHITE_BLUE);
     displayBoot(&h);
     asm("cli");
-    installPs2Handlers(&h);
-    InstallPS2(&h,&_ps2);
-    bool fo = false;
-    bool so = false;
-    if(_ps2.isDualChannled && _ps2.okay){
-        fo = true;
-    }
-    if(_ps2.probeRes[0] == KPR_PASSED && _ps2.probeRes[1] == KPR_PASSED){
-        so = true;
-    }else if(_ps2.probeRes[0] != KPR_PASSED && _ps2.probeRes[1] != KPR_PASSED){
-        so = 0;
-    }else{
-        so = 2;
-    }
-    w = h.printf(h._tb,"PS2: %bb %be ",CC_WHITE_BLUE,fo,so);
-    h._tb+=(w*2);
-    h.newLine(&h._tb,w,CC_WHITE_BLUE);
+    InitalizePS2(&_ps2,&_psd[0],&h);
+    *TEXT_BUFFER = '!';
+    asm("sti");
+    InitalizeKeyboard(&h,&kbd,&_psd[0]);
     for(;;){};
 }
 void displayBoot(StageHandles* sh){
     uint8_t (*onLine)(uint16_t line,char* data,enum _ColorCode cc) = 0x00000acc;
     onLine(23,"Console >>>",CC_WHITE_BLUE);
+}
+void InitalizePS2(PS2Controller* _ps2,PS2Device* _psd,StageHandles* sh){
+    installPs2Handlers(sh);
+    InstallPS2(sh,_ps2);
+    bool fo = false;
+    bool so = false;
+    if(_ps2->isDualChannled && _ps2->okay){
+        fo = true;
+    }
+    if(_ps2->probeRes[0] == KPR_PASSED && _ps2->probeRes[1] == KPR_PASSED){
+        so = true;
+    }else if(_ps2->probeRes[0] != KPR_PASSED && _ps2->probeRes[1] != KPR_PASSED){
+        so = 0;
+    }else{
+        so = 2;
+    }
+    int w = sh->printf(sh->_tb,"PS2: %bb %be ",CC_WHITE_BLUE,fo,so);
+    sh->_tb+=(w*2);
+    sh->newLine(&sh->_tb,w,CC_WHITE_BLUE);
+    w = sh->printf(sh->_tb,"PS2: %x,%x",CC_WHITE_BLUE,_ps2->CRes[0],_ps2->CRes[1]);
+    sh->_tb+=(w*2);
+    sh->newLine(&sh->_tb,w,CC_WHITE_BLUE);
+    detectPS2Devices(sh,_ps2,_psd);
+}
+extern void __kbd__(Registers* regs);
+void InitalizeKeyboard(StageHandles* handles,Keyboard* kbd,PS2Device* devs){
+    prepareKeyboard(handles,devs,kbd);
+    handles->IRQRegisterHandler(1,__kbd__);
+    createDriver(&kbd_drv,kbd,handles);
+}
+
+StageHandles* _getHandles(){
+    return sh;
 }
 void setupHandles(StageHandles* sh,void** _f){
     sh->printf         = _f[0];
@@ -68,6 +96,10 @@ void setupHandles(StageHandles* sh,void** _f){
 void irq12(Registers* regs){
     
 }
+void key(Registers* regs){
+
+}
 void installPs2Handlers(StageHandles* sh){
     sh->IRQRegisterHandler(12,irq12);
+    sh->IRQRegisterHandler(1,key);
 }
