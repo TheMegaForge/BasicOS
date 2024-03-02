b1cfdb85d3e2a2278b541d2620013b609650b682
TheMegaForge <forgingsmeltery06@gmail.com>
2024-03-03 00:05:34 +0100

    Please dont let me edit this ever again!
    added device detection for PS/2

--- os_src/stage2/PS2Controller.c
+++ os_src/stage2/PS2Controller.c
@@ -14,6 +14,13 @@ uint8_t readIO_(StageHandles* sh,uint16_t port){
     }
     return sh->inb_(port);
 }
+uint8_t readIO_Moment(StageHandles* sh,uint16_t port){
+    uint8_t status = sh->inb_(KBD_STATUS_PORT);
+    if(status & 1 == 0){
+        return 0xEE;
+    }
+    return sh->inb_(port);
+}
 uint8_t readCMD(StageHandles* sh,uint8_t cmd){
     writeIO_(sh,KBD_COMMAND_PORT,cmd);
     return readIO_(sh,KBD_DATA_PORT);
@@ -28,7 +35,7 @@ void writeSecondPort(StageHandles* sh,uint8_t data){
     writeIO_(sh,KBD_COMMAND_PORT,0xD4);
     writeIO_(sh,KBD_DATA_PORT,data);
 }
-extern __cdecl bool probeIfRight(uint8_t fv,uint8_t sv);
+//extern __cdecl bool probeIfRight(uint8_t fv,uint8_t sv); -------------unused--------------
 void InstallPS2(StageHandles* sh,PS2Controller* key){
     sh->outb_(KBD_COMMAND_PORT,0xAD);
     sh->outb_(KBD_COMMAND_PORT,0xA7);
@@ -71,13 +78,6 @@ void InstallPS2(StageHandles* sh,PS2Controller* key){
         oldVal|=0b10;
     }
 
-    if(!key->isDualChannled){
-        key->probeRes[1] = KPR_NO_RESULT;
-    }else{
-        writeIO_(sh,KBD_COMMAND_PORT,0xA8);
-        key->probeRes[1] = readCMD(sh,0xA9);
-        oldVal|=0b10;
-    }
     if(key->okay){
         writeIO_(sh,KBD_COMMAND_PORT,0x60);
         writeIO_(sh,KBD_DATA_PORT,oldVal);
@@ -107,3 +107,96 @@ void InstallPS2(StageHandles* sh,PS2Controller* key){
     }   
 }
 
+
+void detectPS2Devices(StageHandles* sh,PS2Controller* cntrl,PS2Device* devices){
+    if(!cntrl->okay){
+        devices[0] = PSD_ERROR;
+        devices[1] = PSD_ERROR;
+        return;
+    }
+    if(!cntrl->isDualChannled){
+        devices[1] = PSD_NO_DEV;
+    }
+    if((readIO_(sh,KBD_STATUS_PORT) & 0b1) == 0b1){
+        readIO_(sh,KBD_DATA_PORT);
+    }
+if(cntrl->CRes[0] == PSC_FAIL){
+    devices[0] = PSD_ERROR;
+    goto dev2;
+}
+before:
+    writeIO_(sh,KBD_DATA_PORT,0xF5);
+    uint8_t retVal = readIO_(sh,KBD_DATA_PORT);
+    if(retVal == 0xFE){
+        goto before;
+    }
+before2:
+    writeIO_(sh,KBD_DATA_PORT,0xF2);
+    retVal = readIO_(sh,KBD_DATA_PORT);
+    if(retVal == 0xFE){
+        goto before2;
+    }
+    int i = 0;
+    uint8_t mem[100];
+    while((readIO_(sh,KBD_STATUS_PORT) & 0b01000000) == 0b00000000 & i != 100){
+        mem[i] = readIO_Moment(sh,KBD_DATA_PORT);
+        i++;
+    }
+    i = 0;
+    while(mem[i] != PSD_BASE && i < 100){
+        i++;
+    }
+before3:
+    devices[0] = mem[i+1];
+    writeIO_(sh,KBD_DATA_PORT,0xF4);
+    for(int i=0;i<10;i++)
+        sh->io_wait();
+    retVal = readIO_(sh,KBD_DATA_PORT);
+    if(retVal == 0xFE){
+        goto before3;
+    }
+//dev 1 ^ 
+dev2:
+    if(cntrl->isDualChannled){
+before4:
+        writeSecondPort(sh,0xF5);
+        retVal = readIO_(sh,KBD_DATA_PORT);
+        if(retVal == 0xFE){
+            goto before4;
+        }
+before5:
+        writeSecondPort(sh,0xF2);        
+        retVal = readIO_(sh,KBD_DATA_PORT);
+        if(retVal == 0xFE){
+            goto before5;
+        }
+        int i = 0;
+        for(int i=0;i<100;i++){
+            mem[i] = 0;
+        }
+        while((readIO_(sh,KBD_STATUS_PORT) & 0b01000000) == 0b00000000 & i != 100){
+            mem[i] = readIO_Moment(sh,KBD_DATA_PORT);
+            i++;
+        }
+        i = 0;
+        while(mem[i] != PSD_BASE && i < 100){
+            if(mem[i] == 0){
+                devices[1] = 0;
+                return;
+            }
+            i++;
+        }
+before6:
+        devices[0] = mem[i+1];
+        writeIO_(sh,KBD_DATA_PORT,0xF4);
+        retVal = readIO_(sh,KBD_DATA_PORT);
+        if(retVal == 0xFE){
+            goto before6;
+        }
+    }
+}
+
+
+void unlockDevices(StageHandles* sh){
+    
+}
