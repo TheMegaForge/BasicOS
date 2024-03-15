6088d020cd0430341ecc308fd637dfde1cc500b7
TheMegaForge <forgingsmeltery06@gmail.com>
2024-03-15 17:18:37 +0100

    remake
    removed parameters

--- os_src/stage2/include/hw/KBDDriver.h
+++ os_src/stage2/include/hw/KBDDriver.h
@@ -1,19 +1,13 @@
 #pragma once
 #include "../SwitchHandles.h"
 #include <stdint.h>
-//typedef void(*KBDPlug)(StageHandles* sh);
+//typedef void(*KBDPlug)( );
 typedef int8_t char8_t;
 typedef struct{
-    uint8_t rep[100];
-    uint8_t buf[100];
-    bool held;
-    bool output;
-    uint8_t curr;
-    uint8_t lread;
-    uint8_t read;
+    uint8_t _char;
     bool active;
 }KBDDriver;
-char await(StageHandles* sh);
-void fgets(StageHandles* sh,char* location,uint8_t length);
-void fgets_tui(StageHandles* sh,char* location,uint8_t  length,char** tb,enum _ColorCode cc);
-void fgets_opi(StageHandles* sh,char* location,uint8_t* cnt,char** tb,enum _ColorCode cc); // waits until enter key is pressed
+char await();
+void fgets(char* location,uint8_t length);
+void fgets_tui(char* location,uint8_t  length,char** tb,enum _ColorCode cc);
+void fgets_opi(char* location,uint8_t* cnt,char** tb,enum _ColorCode cc); // waits until enter key is pressed
